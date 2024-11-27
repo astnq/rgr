598e2f812d622d66099ed84cec1c1eab04325d74
ksanax <163411035+astnq@users.noreply.github.com>
2024-11-27 21:00:35 +0300

    Update rc6.h

--- chiipes-system/rc6.h
+++ chiipes-system/rc6.h
@@ -10,8 +10,11 @@ using namespace std;
 inline uint32_t ROTL(uint32_t x, uint32_t y);
 inline uint32_t ROTR(uint32_t x, uint32_t y);
 void keySchedule(const vector<uint8_t>& key);
-void encrypt(uint32_t& A, uint32_t& B, uint32_t& C, uint32_t& D);
-void decrypt(uint32_t& A, uint32_t& B, uint32_t& C, uint32_t& D);
+void Encryption(uint32_t& A, uint32_t& B, uint32_t& C, uint32_t& D);
+void Decryption(uint32_t& A, uint32_t& B, uint32_t& C, uint32_t& D);
+vector<char> checkinputrc6(const string& message);
+string rc6Encryption(string& message);
+string rc6Decryption(const string& message);
 
 #include "rc6.cpp"
 
