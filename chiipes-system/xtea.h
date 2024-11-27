78f2090393bfb7dcb5378d80546fe2ef2009d90c
ksanax <163411035+astnq@users.noreply.github.com>
2024-11-27 21:00:56 +0300

    Update xtea.h

--- chiipes-system/xtea.h
+++ chiipes-system/xtea.h
@@ -6,11 +6,14 @@
 #include <vector>
 #include <cstring>
 
-void xteaEncrypt(uint32_t v[2], const uint32_t key[4]);
-void xteaDecrypt(uint32_t v[2], const uint32_t key[4]);
+void xteaEncryption(uint32_t v[2], const uint32_t key[4]);
+void xteaDecryption(uint32_t v[2], const uint32_t key[4]);
 vector<uint32_t> text_to_blocks(const string& text);
-string blocks_to_text(const vector<uint32_t>& blocks)
-
+string blocks_to_text(const vector<uint32_t>& blocks);
+vector<char> checkinputxtea(const string& message);
+void generateXteaKeys();
+string xteaEncryption(string& message);
+string xteaDecryption(const string& message);
 
 
 
