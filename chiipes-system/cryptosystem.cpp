5f800065960278a154d40165993951290831b5ed
ksanax <163411035+astnq@users.noreply.github.com>
2024-12-06 21:50:13 +0300

    Update cryptosystem.cpp

--- chiipes-system/cryptosystem.cpp
+++ chiipes-system/cryptosystem.cpp
@@ -81,22 +81,24 @@ void hughes_decrypt(unsigned char* data, unsigned char* key, size_t data_len) {
 }
 
 int main() {
-    system("cls");
+     system("cls");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     srand(time(0));
 
     string password;
-    cout << "Введите пароль: ";
+    const string passwd = "1105"; // Укажите пароль
+    cout << "Enter the password: ";
     cin >> password;
+
     if (password != passwd) {
-        cout << "Пароль неверный! Попробуйте еще раз: ";
+        cout << "Incorrect password! Try again: ";
         cin >> password;
-        if (password != passwd) {
-            cout << "Вы вводите неверный пароль! Последняя попытка: ";
+        if (password != passwd) {   
+            cout << "You are entering the wrong password! Last attempt: ";
             cin >> password;
             if (password != passwd) {
-                cout << "Вы использовали все попытки! Завершение программы...";
+                cout << "All attempts used! Exiting the program...";
                 exit(0);
             }
         }
@@ -105,89 +107,99 @@ int main() {
     string user_input;
     unsigned char key[16] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
 
-    // Ввод строки пользователем
-    cout << "Enter a string to encrypt: ";
-    getline(cin, user_input);
+    // Цикл для повторяющихся запросов на выбор шифра и ввод строки
+    while (true) {
+        int choice;
+        cout << "\nWelcome!\n" << "------------------------------\n";
+        cout << "1. RC6\n";
+        cout << "2. XTEA\n";
+        cout << "3. Hughes\n";
+        cout << "0. Exit the program\n";
+        cout << "------------------------------\n";
+        cout << "\nSelect the cipher number: ";
+        cin >> choice;
 
-    unsigned char* data = (unsigned char*)user_input.c_str();
-    size_t data_len = user_input.length();  
-
-    int choice;
-    cout << "Welcome!\n" << "------------------------------\n";
-    cout << "1. RC6\n";
-    cout << "2. XTEA\n";
-    cout << "3. Hughes\n";
-    cout << "0. Exit the program\n"
-    << "------------------------------\n";
-    cout << "\nSelect the cipher number: ";
-    cin >> choice;
-
-    // Использование vector вместо массива
-    vector<unsigned char> data_copy(data, data + data_len);
-
-    // Шифрование
-    switch (choice) {
-        case 1:
-            cout << "Encrypting using RC6...\n";
-            rc6_encrypt(data_copy.data(), key, data_len);
+        // Выход из программы
+        if (choice == 0) {
+            cout << "Exiting the program...\n";
             break;
-        case 2:
-            cout << "Encrypting using XTEA...\n";
-            xtea_encrypt(data_copy.data(), key, data_len);
-            break;
-        case 3:
-            cout << "Encrypting using Hughes...\n";
-            hughes_encrypt(data_copy.data(), key, data_len);
-            break;
-        default:
-            cout << "Invalid choice.\n";
-            return 1;
-    }
-
-    cout << "Encrypted data: ";
-    printHex(data_copy.data(), data_len);
-
-    // Расшифровка
-    switch (choice) {
-        case 1:
-            cout << "Decrypting using RC6...\n";
-            rc6_decrypt(data_copy.data(), key, data_len);
-            break;
-        case 2:
-            cout << "Decrypting using XTEA...\n";
-            xtea_decrypt(data_copy.data(), key, data_len);
-            break;
-        case 3:
-            cout << "Decrypting using Hughes...\n";
-            hughes_decrypt(data_copy.data(), key, data_len);
-            break;
-        default:
-            cout << "Invalid choice.\n";
-            return 1;
-    }
-
-    cout << "Decrypted data: ";
-    printHex(data_copy.data(), data_len);
-    cout << "Decrypted text: " << data_copy.data() << endl;
-
-    // Запись в файл (зашифрованный и расшифрованный текст)
-    ofstream outfile("encrypted_decrypted_data.txt", ios::binary);
-    if (outfile) {
-        // Запись зашифрованных данных
-        outfile << "Encrypted data (in hex):\n";
-        for (size_t i = 0; i < data_len; i++) {
-            outfile << hex << setw(2) << setfill('0') << (int)data_copy[i] << " ";
         }
-        outfile << endl;
 
-        // Запись расшифрованных данных
-        outfile << "\nDecrypted data (in text): " << data_copy.data() << endl;
+        cout << "Enter a string to encrypt: ";
+        cin.ignore();  // Для очистки буфера после выбора
+        getline(cin, user_input);
 
-        cout << "Encrypted and decrypted data saved to 'encrypted_decrypted_data.txt'.\n";
-    } else {
-        cout << "Error opening file for writing.\n";
+        unsigned char* data = (unsigned char*)user_input.c_str();
+        size_t data_len = user_input.length();
+
+        // Использование vector вместо массива
+        vector<unsigned char> data_copy(data, data + data_len);
+
+        // Шифрование
+        switch (choice) {
+            case 1:
+                cout << "Encrypting using RC6...\n";
+                rc6_encrypt(data_copy.data(), key, data_len);
+                break;
+            case 2:
+                cout << "Encrypting using XTEA...\n";
+                xtea_encrypt(data_copy.data(), key, data_len);
+                break;
+            case 3:
+                cout << "Encrypting using Hughes...\n";
+                hughes_encrypt(data_copy.data(), key, data_len);
+                break;
+            default:
+                cout << "Invalid choice.\n";
+                continue; // Повторить запрос выбора шифра
+        }
+
+        cout << "Encrypted data: ";
+        printHex(data_copy.data(), data_len);
+
+        // Расшифровка
+        switch (choice) {
+            case 1:
+                cout << "Decrypting using RC6...\n";
+                rc6_decrypt(data_copy.data(), key, data_len);
+                break;
+            case 2:
+                cout << "Decrypting using XTEA...\n";
+                xtea_decrypt(data_copy.data(), key, data_len);
+                break;
+            case 3:
+                cout << "Decrypting using Hughes...\n";
+                hughes_decrypt(data_copy.data(), key, data_len);
+                break;
+            default:
+                cout << "Invalid choice.\n";
+                continue; // Повторить запрос выбора шифра
+        }
+
+        cout << "Decrypted data: ";
+        printHex(data_copy.data(), data_len);
+        cout << "Decrypted text: " << data_copy.data() << endl;
+
+        // Запись в файл (зашифрованный и расшифрованный текст)
+        ofstream outfile("encrypted_decrypted_data.txt", ios::binary);
+        if (outfile) {
+            // Запись зашифрованных данных
+            outfile << "Encrypted data (in hex):\n";
+            for (size_t i = 0; i < data_len; i++) {
+                outfile << hex << setw(2) << setfill('0') << (int)data_copy[i] << " ";
+            }
+            outfile << endl;
+
+            // Запись расшифрованных данных
+            outfile << "\nDecrypted data (in text): " << data_copy.data() << endl;
+
+            cout << "Encrypted and decrypted data saved to 'encrypted_decrypted_data.txt'.\n";
+        } else {
+            cout << "Error opening file for writing.\n";
+        }
     }
 
     return 0;
 }
+
 
